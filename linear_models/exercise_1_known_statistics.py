a581a0a7135543b8111d2e8453db5f5320bb685a
Stefan de Lange <langestefan@msn.com>
2022-05-01 16:15:37 +0200

    Finish newton's method
    Signed-off-by: Stefan de Lange <langestefan@msn.com>

--- linear_models/exercise_1_known_statistics.py
+++ linear_models/exercise_1_known_statistics.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error
 from scipy.ndimage.interpolation import shift
 
 
-# do SGD algorithm for N iterations  
+# compute SGD algorithm for N iterations  
 # w[k+1] = w[k] + 2*alpha*(r_yx - R_x*w[k])
 def steepest_gradient_descent(R_x, r_yx, alpha, max_iter, w_init):
     """ 
@@ -32,8 +32,56 @@ def steepest_gradient_descent(R_x, r_yx, alpha, max_iter, w_init):
         w_history[[i], :] = w.T
 
     return w_history
-    
 
+# compute Newton's method for N iterations
+# w[k+1] = w[k] + 2*alpha*(R_x^-1)*(r_yx - R_x*w[k])
+def newtons_method(R_x, r_yx, alpha, max_iter, w_init):
+    """ 
+    :param np.array(3, 3) R_x: autocorrelation matrix
+    :param np.array r_yx(3, 1): cross-correlation vector
+    :param float alpha: learning rate
+    :param int max_iter: number of iterations
+    :param np.array w_init(3, 1): initial weight vector
+    """
+    # initialize weights
+    w = w_init
+
+    # initialize weights history
+    w_history = np.zeros((max_iter, 3))
+
+    # compute R_x^-1
+    R_x_inv = np.linalg.inv(R_x)
+
+    # compute newtons method
+    for i in range(max_iter):
+        # update weights
+        w = w + 2*alpha*np.dot(R_x_inv, (r_yx - np.dot(R_x, w)))
+
+        # store weights
+        w_history[[i], :] = w.T
+
+    return w_history
+
+# contour plot function
+def contour_plot(w0, w1, w_train, J_vals, title, filename):
+
+    # plot the contour plot
+    fig = plt.figure()
+    cp = plt.contour(w0, w1, J_vals)
+    plt.clabel(cp, inline=1, fontsize=10)
+    plt.plot(w_train[:,0], w_train[:,1])
+    plt.title(title)
+    plt.xlabel('w0')
+    plt.ylabel('w1')
+
+    # plot optimal weights as point in contourplot
+    plt.plot(0.2, 1, 'x', color='red', markersize=10)
+    plt.show()
+
+
+    # save figure as png
+    figure_name = f"linear_models/figures/{filename}.png"
+    fig.savefig(figure_name, dpi=300)
 
 if __name__ == "__main__":
     print(" ---- start ---- ")
@@ -50,7 +98,7 @@ if __name__ == "__main__":
     w_3_const = -0.5 # for countourplots
     w_init = np.zeros((3, 1))
     N = data.shape[0]
-    alpha = 0.05
+    alpha = 0.1
 
     # autocorrelation and cross-correlation matrices
     r_yx = np.array([[1, 5.3, -3.9]]).T 
@@ -75,25 +123,19 @@ if __name__ == "__main__":
             w_temp = np.array([W0[0, i], W1[j, 0], w_3_const]).T
             J_vals[i, j] =  mean_squared_error(data_y, np.dot(x_stack, w_temp))
 
-    # plot the contour plot
-    fig = plt.figure()
-    cp = plt.contour(w0, w1, J_vals)
-    plt.clabel(cp, inline=1, fontsize=10)
-    plt.plot(w_sgd[:,0], w_sgd[:,1])
-    plt.title('SGD Contour Plot for alpha = {}'.format(alpha))
-    plt.xlabel('w0')
-    plt.ylabel('w1')
-    plt.show()
+    # plot the contour plot for SGD
+    contour_plot(W0, W1, w_sgd, J_vals, 
+        title=f"SGD Contour Plot for alpha = {alpha}", 
+        filename=f"sgd_contour_plot_alpha_{alpha}")
 
-    # save figure as png
-    figure_name = "linear_models/sgd_contour_plot_alpha_{}.png".format(alpha)
-    fig.savefig(figure_name, dpi=300)
+    # apply newtons method
+    w_newton = newtons_method(R_x, r_yx, alpha, N, w_init)
 
+    # plot the contour plot for Newton's method
+    contour_plot(W0, W1, w_newton, J_vals,
+        title=f"Newton's Method Contour Plot for alpha = {alpha}",
+        filename=f"newtons_method_contour_plot_alpha_{alpha}")
 
-
-    
-
-    
 
 
 

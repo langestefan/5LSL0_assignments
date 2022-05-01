6620f2702d08494036ad28f7c682611190a7f23f
Stefan de Lange <langestefan@msn.com>
2022-05-01 15:33:04 +0200

    Add file for excercise 2

---
+++ linear_models/exercise_2_unknown_statistics.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib   as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from sklearn.metrics import mean_squared_error
+from scipy.ndimage.interpolation import shift
+
+
+
+
+if __name__ == "__main__":
+    print(" ---- start ---- ")

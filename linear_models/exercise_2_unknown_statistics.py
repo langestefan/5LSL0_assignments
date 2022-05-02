9980aa9550646ae9f67be5754a88e09fea787935
Tong <y.tong@student.tue.nl>
2022-05-02 14:32:00 +0200

    update RLS.

--- linear_models/exercise_2_unknown_statistics.py
+++ linear_models/exercise_2_unknown_statistics.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import mean_squared_error
-from scipy.ndimage.interpolation import shift
+from scipy.ndimage import shift
 
 
 # compute LMS algorithm for N iterations  
@@ -21,7 +21,7 @@ def Least_Mean_Square(x_stack, y, alpha, max_iter, w):
         weight = w[i-1].T
         x_k = x_stack[i].T
         y_hat = np.dot(weight.T,x_k)
-        error = data_y[i]-y_hat
+        error = y[i]-y_hat
         #update weights
         weight_new = weight + 2*alpha*np.dot(x_k,error)
         #stroe weight
@@ -45,7 +45,7 @@ def Normlized_LMS(x_stack, y, alpha, max_iter, w):
         weight = w[i-1].T
         x_k = x_stack[i].T
         y_hat = np.dot(weight.T,x_k)
-        error = data_y[i]-y_hat
+        error = y[i]-y_hat
         sigma = np.dot(x_k,x_k.T)/3 + 0.01
         #update weights
         weight_new = weight + 2*(alpha/sigma)*np.dot(x_k,error)
@@ -54,6 +54,38 @@ def Normlized_LMS(x_stack, y, alpha, max_iter, w):
 
     return w
 
+# compute RLS algorithm for N iterations  
+# w[k+1] = np.dot(inv(R_x_hat), r_yx_hat)
+def RLS(x_stack, y, gamma, delta_inv, max_iter, w):
+    """
+    :param np.arryr x_stack : x[k]
+    :param np.array y : reference data_y
+    :param float gamma: forgetting factor
+    :param float delta_inv: inverse of the auto-correlation matrix of x[k]
+    :param int max_iter: number of iterations
+    :param np.array w : weight vector
+    """
+    # parameter initialization
+    R_x_inv = delta_inv * np.identity(3)
+    r_yx_hat = np.zeros((3,1))
+
+        # compute RLS
+    for i in range(1, max_iter):
+        
+        x_k = x_stack[i].T        
+        g = np.dot(R_x_inv, x_stack[i]) / (gamma**2 + np.dot(np.dot(x_k.T, R_x_inv), x_k))
+        R_x_inv = gamma**(-2) * (R_x_inv - np.dot(np.dot(g, x_k.T), R_x_inv))
+        r_yx_pre = np.array([np.dot(x_k.T, y[i])])
+        r_yx_hat = np.multiply(gamma**2 , r_yx_hat)  + r_yx_pre.T
+        
+        #update weights
+        weight_new = np.dot(R_x_inv,r_yx_hat)
+       
+        #stroe weight
+        w[i] = weight_new.T   
+
+    return w
+    
 # contour plot function
 def contour_plot(w0, w1, w_train, J_vals, title, filename):
 
@@ -92,14 +124,21 @@ if __name__ == "__main__":
     w_3_const = -0.5 # for countourplots    
     N = data.shape[0]
     w = np.zeros((N, 3))
+
+    # set parameters for each algorithm
     alpha = 0.0001
+    gamma = 1 - 1e-4
+    delta_inv = 0.01
     
         
-    # apply mean square error
+    """    # apply mean square error
     w_lms = Least_Mean_Square(x_stack, data_y, alpha, N, w)
 
     # apply normalized mean square error
-    w_nlms = Normlized_LMS(x_stack, data_y, alpha, N, w)
+    w_nlms = Normlized_LMS(x_stack, data_y, alpha, N, w) """
+
+    # apply RLS
+    w_RLS = RLS(x_stack, data_y, gamma, delta_inv, N, w)
 
     # Plot the trajectory of the filter coefficients as they evolve, together with a contour
     # plot of the objective function J. 
@@ -114,13 +153,17 @@ if __name__ == "__main__":
             w_temp = np.array([W0[0, i], W1[j, 0], w_3_const]).T
             J_vals[i, j] =  mean_squared_error(data_y, np.dot(x_stack, w_temp))
 
-    # plot the contour plot for LMS
+    """     # plot the contour plot for LMS
     contour_plot(W0, W1, w_lms, J_vals, 
         title=f"LMS Contour Plot for alpha = {alpha}", 
         filename=f"LMS_contour_plot_alpha_{alpha}")
-    
+
     # plot the contour plot for NLMS
     contour_plot(W0, W1, w_nlms, J_vals,
         title=f"NLMS Contour Plot for alpha = {alpha}",
         filename=f"NLMS_contour_plot_alpha_{alpha}")
-
+    """
+    # plot the contour plot for RLS
+    contour_plot(W0, W1, w_RLS, J_vals,
+        title=f"RLS Contour Plot for delta_inv = {delta_inv}",
+        filename=f"RLS_contour_plot_delta_inv_{delta_inv}")

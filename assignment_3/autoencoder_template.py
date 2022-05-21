77799ba20a8048737f2d5f88cdaf60bd8a5475d5
Tong <y.tong@student.tue.nl>
2022-05-21 16:28:41 +0200

    finish AE.

--- assignment_3/autoencoder_template.py
+++ assignment_3/autoencoder_template.py
@@ -12,16 +12,16 @@ class Encoder(nn.Module):
             # input is N, 1, 32, 32
             nn.Conv2d(in_channels = 1, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 'same'), # N, 16, 32, 32
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2), # N, 16, 16, 16
+            nn.MaxPool2d((2,2), stride=2), # N, 16, 16, 16
             nn.Conv2d(in_channels =16, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 'same'), # N, 16, 16, 16
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2), # N, 16, 8, 8
+            nn.MaxPool2d((2,2), stride=2), # N, 16, 8, 8
             nn.Conv2d(in_channels =16, out_channels = 16,kernel_size = (3, 3), stride = 1, padding = 'same'), # N, 16, 8, 8
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2), # N, 16, 4, 4
+            nn.MaxPool2d((2,2), stride=2), # N, 16, 4, 4
             nn.Conv2d(in_channels =16, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 'same'), # N, 16, 4, 4
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2), # N, 16, 2, 2
+            nn.MaxPool2d((2,2), stride=2), # N, 16, 2, 2
             nn.Conv2d(in_channels =16, out_channels = 1, kernel_size = (2, 2), stride = 1, padding = 'same'), # N, 1, 2, 2
             nn.ReLU(True),
             nn.MaxPool2d((1,2), stride=1), # N, 1, 2, 1
@@ -38,15 +38,18 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         #input is N, 1, 2, 1
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels = 1, out_channels = 16, kernel_size = (2, 2), stride = 1, padding = 'same'), # N, 16, 2, 2
+            nn.ConvTranspose2d(in_channels = 1, out_channels = 16, kernel_size = (1, 2), stride = 1, padding = 0), # N, 16, 2, 2
             nn.ReLU(True),
-            nn.UpsamplingNearest2d(scale_factor = 2), # N, 16, 4, 4
-            nn.ConvTranspose2d(in_channels = 16, out_channels = 16, kernel_size = (3, 3), stride = 2, padding = 'same'), # N, 32, 20, 20
-            #nn.ReLU(True),
-            #nn.ConvTranspose2d(in_channels = 64, out_channels = 32, kernel_size = (3, 3), stride = 2, padding = 'same'), # N, 32, 40, 40
-            #nn.ReLU(True),
-            #nn.ConvTranspose2d(in_channels = 32, out_channels = 16, kernel_size = (3, 3), stride = 2, padding = 'same'), # N, 16, 80, 80
-            #nn.ReLU(True),
+            nn.UpsamplingNearest2d(scale_factor = (2,2)), # N, 16, 4, 4
+            nn.ConvTranspose2d(in_channels = 16, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 1), # N, 16, 4, 4
+            nn.ReLU(True),
+            nn.UpsamplingNearest2d(scale_factor = (2,2)), # N, 16, 8, 8
+            nn.ConvTranspose2d(in_channels = 16, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 1), # N, 16, 8, 8
+            nn.ReLU(True),
+            nn.UpsamplingNearest2d(scale_factor = (2,2)), # N, 16, 16, 16
+            nn.ConvTranspose2d(in_channels = 16, out_channels = 16, kernel_size = (3, 3), stride = 1, padding = 1), # N, 16, 16, 16
+            nn.ReLU(True),
+            nn.UpsamplingNearest2d(scale_factor = (2,2)), # N, 16, 32, 32
             #nn.ConvTranspose2d(in_channels = 16, out_channels = 1, kernel_size = 3, stride = 2, padding = 'same'), # N, 1, 160, 240
             #nn.Tanh()
         )
@@ -74,10 +77,15 @@ def test():
     #model = Encoder()
     
     # test decoder part
-    x = torch.randn((1, 1, 2, 1))
-    model = Decoder()
+    #x = torch.randn((1, 1, 2, 1))
+    #model = Decoder()
 
-    preds = model(x)
+    # test AE
+    x = torch.randn((16, 1, 32, 32))   
+    model = AE()
+
+    preds , latent = model(x)
+    print('latent:',latent.shape)
     print ('preds :',preds.shape)
     print ('x :',x.shape)
 

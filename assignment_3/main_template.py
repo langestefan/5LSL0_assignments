2dd25535f030191a2ca0b3954c60733df219f74f
Tong <y.tong@student.tue.nl>
2022-05-23 20:51:47 +0200

    add testing process.

--- assignment_3/main_template.py
+++ assignment_3/main_template.py
@@ -1,10 +1,8 @@
 # libraries
-from turtle import shape
 import torch.optim as optim
 import torch.nn as nn
 import torch
 import numpy as np
-from sklearn.decomposition import PCA
 
 # from tqdm import tqdm
 from tqdm.auto import tqdm as tqdm
@@ -15,33 +13,23 @@ import MNIST_dataloader
 import autoencoder_template
 import train
 
-def scatter_plot(model, x_clean_test, labels_train):
+def scatter_plot(mnist_points):
+    """
+    Plot function from assignment document
+    :param hw_points: Handwritten data feature vectors (digits, points, (x0, y0)) = (10, 1, ndim)
+    :param mnist_points: MNIST feature vectors (digits, points, (x0, y0)) = (10, 20, ndim)
+    """
+    colors = plt.cm.Paired(np.linspace(0, 1, len(mnist_points)))
+    fig, ax = plt.subplots(figsize=(7, 5))
 
-
-
-    # PCA  
-    #vector_feature= [ fv.detach().numpy() for fv in vector_feature]                                                    
-    pca = PCA(n_components=2)
-    vector_feature = pca.fit_transform(latent)
-     
-    
-    all_points = []
-    for i in range(10):
-        all_point = []
-        for index,label in enumerate(labels_train):
-            if label.item()==i and len(all_point)<20:
-                all_point.append(vector_feature[index].tolist())
-        all_points.append(all_point)   
-    
-    colors = plt.cm.Paired(np.linspace(0,1,len(all_points)))
-    fig, ax = plt.subplots()
-    for (points, color, digit) in zip (all_points, colors, range (10)):
+    for (points, color, digit) in zip(mnist_points, colors, range(10)):
         ax.scatter([item[0] for item in points],
-                    [item[1] for item in points],
-                    color = color, label = 'digit{}'.format(digit))
+                   [item[1] for item in points],
+                   color=color, label='digit{}'.format(digit))
+
+
     ax.grid(True)
-    plt.title('scatter plot') 
-    ax.legend(loc='best')
+    ax.legend()
     plt.show()
 
 def load_model(model, filename):
@@ -74,44 +62,88 @@ def plot_images_exercise_1(x_data, recon):
     #plt.savefig("exercise_1.png",dpi=300,bbox_inches='tight')
     plt.show() 
 
-# set torches random seed
-torch.random.manual_seed(0)
+def test_model(model, test_loader, device):
+    """ Test the trained model.
+    Args:
+        model (Model class): Trained model to test.
+        x_test (torch.Tensor): Test data.
+        y_test (torch.Tensor): Test labels.
+    Returns:
+        float: Accuracy of the model on the test data.
+    """
+   
+    test_loss = 0
+    # go over all minibatches
+    for batch_idx,(x_clean, x_noisy, test_label) in enumerate(tqdm(test_loader, position=0, leave=False, ascii=False)):
+        # fill in how to train your network using only the clean images
 
-# define parameters
-data_loc = 'data' #change the data location to something that works for you
-batch_size = 64
-n_epochs = 1
-learning_rate = 3e-4
+        # move to device
+        x_clean = x_clean.to(device)
 
-# get dataloader
-train_loader,valid_loader, test_loader = MNIST_dataloader.create_dataloaders(data_loc, batch_size)
-
-x_clean_test  = test_loader.dataset.Clean_Images
-
-labels_test   = test_loader.dataset.Labels
-
-# create the autoencoder
-AE = autoencoder_template.AE()
-
-# load the trained model 
-AE = load_model(AE, "AE_model_params.pth")
-
-# create the optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(AE.parameters(), learning_rate, weight_decay=1e-5)
+        # forward pass
+        recon, latent = model(x_clean)
+        
+        latent = latent.detach().cpu()
+        
+    
+        
+        test_loss = criterion(recon, x_clean)
+        # add loss to the total loss
+        test_loss += test_loss.item()
 
 
+    # print the average loss for this epoch
+    test_losses = test_loss / len(test_loader)
 
-# define the device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #"cpu"
-print("Using device:", device)
+    print(f"Test loss is {test_losses}.")
+    #test_losses = test_losses.detech().cpu()
+    recon = recon.detach().cpu()
+    
 
-# move model to device
-AE.to(device)
+    return test_losses,recon,latent,test_label
 
-# %% training loop
-AE, train_losses, valid_losses = train.train_model (AE, train_loader, valid_loader, optimizer, criterion, n_epochs, device, write_to_file=True)
-#scatter_plot(AE, x_clean_test, labels_test)
+
+
+if __name__ == "__main__":
+
+    # set torches random seed
+    torch.random.manual_seed(0)
+
+    # define parameters
+    data_loc = 'data' #change the data location to something that works for you
+    batch_size = 64
+    n_epochs = 10
+    learning_rate = 3e-4
+
+    # get dataloader
+    train_loader, valid_loader, test_loader = MNIST_dataloader.create_dataloaders(data_loc, batch_size)
+
+    # create the autoencoder
+    AE = autoencoder_template.AE()
+
+    # load the trained model 
+    AE = load_model(AE, "AE_model_params.pth")
+
+    # create the optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(AE.parameters(), learning_rate, weight_decay=1e-5)
+
+
+
+    # define the device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #"cpu"
+    print("Using device:", device)
+
+    # move model to device
+    AE.to(device)
+
+    # %% training loop
+    #AE, train_losses, valid_losses = train.train_model (AE, train_loader, valid_loader, optimizer, criterion, n_epochs, device, write_to_file=True)
+    test_losses,test_recon, test_latent, test_label= test_model(AE, test_loader, device)
+    
+    scatter_plot(test_latent)
+ 
+
 
 """ # # move back to cpu    
 recon = recon.detach().cpu()

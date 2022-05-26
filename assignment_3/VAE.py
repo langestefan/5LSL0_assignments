d86733eddf064a8548d3a906bc7c49a2a8263d2f
Tong <y.tong@student.tue.nl>
2022-05-27 00:30:10 +0200

    halfway of VAE encoder part.

---
+++ assignment_3/VAE.py
@@ -0,0 +1,110 @@
+# imports
+from audioop import lin2adpcm
+from sklearn import linear_model
+import torch
+import torch.nn as nn
+
+
+# Encoder
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        # create layers here
+        self.encoder = nn.Sequential(
+            # input is N, 1, 32, 32
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(4, 4), stride=2, padding=1), # N, 16, 16, 16
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(4, 4), stride=2, padding=1), # N, 16, 8, 8
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(4, 4), stride=2, padding=1), # N, 16, 4, 4
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(5,4), stride=1, padding=(1,0)), # N, 16, 2, 1
+            nn.Flatten(),
+            # Linear layer to output the mean and the std of the latent space
+            nn.Linear(in_features=16, out_features=2),
+            x_mean = nn.linear(in_features=64, out_features=2),
+            x_std = nn.linear(in_features=64, out_features=2)
+
+
+        )
+        
+    def sampling (x_mean,log_x_std):
+        # sampling from the latent space
+        epsilon = torch.randn_like(x_mean)
+        x_std = torch.exp(log_x_std)
+        x_sample = x_mean + x_std * epsilon
+        return x_sample
+
+    def forward(self, x):
+        # use the created layers here
+        h = self.encoder(x)
+        return h
+    
+# Decoder
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        #input is N, 1, 2, 1
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=1, out_channels=16, kernel_size=(1, 1), stride=1, padding=0), # N, 16, 2, 1
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=(1,2), mode='nearest'), # N, 16, 2, 2
+            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding=1), # N, 16, 2, 2
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=(2,2), mode='nearest'), # N, 16, 4, 4
+            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding=1), # N, 16, 4, 4
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=(2,2), mode='nearest'), # N, 16, 8, 8
+            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding=1), # N, 16, 8, 8
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=(2,2), mode='nearest'), # N, 16, 16, 16
+            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding=1), # N, 1, 16, 16
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=(2,2), mode='nearest'), # N, 16, 32, 32
+            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(3, 3), stride=1, padding=1), # N, 1, 32, 32
+            #nn.BatchNorm2d(1),
+            #nn.ReLU(True)
+        )
+        
+    def forward(self, h):
+        # use the created layers here
+        r = self.decoder(h)
+        return r
+    
+# Autoencoder
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        
+    def forward(self, x):
+        h = self.encoder(x)
+        r = self.decoder(h)
+        return r, h
+
+
+
+def test():
+    # test encoder part 
+    x = torch.randn((1, 1, 32, 32))   
+    model = Encoder()
+    
+    # test decoder part
+    #x = torch.randn((1, 1, 2, 1))
+    #model = Decoder()
+
+    # test AE
+    #x = torch.randn((64, 1, 32, 32))   
+    #model = AE()
+
+    #preds, latent = model(x)
+    preds = model(x)
+    #print('latent:', latent.shape)
+    print ('preds :',preds.shape)
+    print ('x :',x.shape)
+
+if __name__ == "__main__":
+    test()    

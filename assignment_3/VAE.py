2849f603bbf00a815b5884306761e01e765aa9c8
Tong <y.tong@student.tue.nl>
2022-05-27 14:14:20 +0200

    rename variables for VAE.py to avoid misunderstand.

--- assignment_3/VAE.py
+++ assignment_3/VAE.py
@@ -80,8 +80,8 @@ class VAE(nn.Module):
         
     def forward(self, x):
         x_sample, x_mean, x_log_var = self.encoder(x)
-        output_encoder = self.decoder(x_sample)
-        return output_encoder, x_sample, x_mean, x_log_var
+        output_decoded = self.decoder(x_sample)
+        return output_decoded, x_sample, x_mean, x_log_var
 
 
 
@@ -109,9 +109,9 @@ def test():
     x = torch.randn((64, 1, 32, 32))   
     model = VAE()
 
-    output_encoder, x_sample, x_mean, x_log_varpreds = model(x)
+    output_decode, x_sample, x_mean, x_log_varpreds = model(x)
     print ('x_input:', x.shape)
-    print('output_encoder', output_encoder.shape)
+    print('output_decode', output_decode.shape)
     print('x_sample', x_sample.shape)
     print('x_mean', x_mean.shape)
     print('x_log_var', x_log_varpreds.shape)

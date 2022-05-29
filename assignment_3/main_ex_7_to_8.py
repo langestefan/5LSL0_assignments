5db89aaaaa2efd6c9aafbdf25a4f3681fc6b75ea
Tong <y.tong@student.tue.nl>
2022-05-29 02:34:35 +0200

    add code for exercise 8a.

--- assignment_3/main_ex_7_to_8.py
+++ assignment_3/main_ex_7_to_8.py
@@ -25,6 +25,37 @@ import VAE
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
+
+def plot_images_exercise_8(model, test_loader):
+   
+    examples = enumerate(test_loader)
+    _, (x_clean_example, x_noisy_example, labels_example) = next(examples)
+
+    output_decoder, x_sample, x_mean, x_log_var  = model(x_clean_example.cuda())
+    output_decoder = output_decoder.data.cpu().numpy()
+
+    plt.figure(figsize=(12, 4))
+    for i in range(10): 
+        plt.subplot(3, 10, i+1)
+        plt.imshow(x_noisy_example[i, 0, :, :], cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.subplot(3, 10, i+10+1)
+        plt.imshow(output_decoder[i, 0, :, :], cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.subplot(3, 10, i+20+1)
+        plt.imshow(x_clean_example[i, 0, :, :], cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+
+    plt.tight_layout()
+    plt.savefig(f'assignment_3/figures/exercise_8_b.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+
 def scatter_plot(latent_tensor, label_tensor, n_points=10000):
     """
     Plot function from assignment document
@@ -71,10 +102,10 @@ def plot_images_exercise_7a(x_data, model_output):
         plt.yticks([])
 
     plt.tight_layout()
-    #plt.savefig("exercise_1.png",dpi=300,bbox_inches='tight')
+    # plt.savefig("exercise_1.png",dpi=300,bbox_inches='tight')
     plt.show()
 
-def plot_mnist_grid_excercise_5(images, n_img_x, n_img_y, save_path=None):
+def plot_mnist_grid_excercise_7d(images, n_img_x, n_img_y, save_path=None):
     """
     Plot MNIST images in a grid
     Args:
@@ -91,7 +122,7 @@ def plot_mnist_grid_excercise_5(images, n_img_x, n_img_y, save_path=None):
             plt.yticks([])
 
     plt.tight_layout()
-    plt.savefig(f'{save_path}/exercise_7d.png', dpi=300, bbox_inches='tight')
+    plt.savefig(f'{save_path}/exercise_7d_1.png', dpi=300, bbox_inches='tight')
     plt.show()
 
 if __name__ == "__main__":
@@ -99,7 +130,7 @@ if __name__ == "__main__":
     torch.random.manual_seed(0)
     # define parameters
     data_loc = 'assignment_3/data' #change the data location to something that works for you
-    batch_size = 16
+    batch_size = 64
     n_epochs = 5
     learning_rate = 1e-3
 
@@ -111,6 +142,7 @@ if __name__ == "__main__":
 
     # # load the trained model
     model = train_ex_7_to_8.load_model(model, "assignment_3/models/VAE_35_epochs.pth")
+    
 
     # create the optimizer
     criterion = nn.MSELoss()
@@ -130,15 +162,15 @@ if __name__ == "__main__":
     #                                                                                                 criterion, n_epochs, device,
     #                                                                                                 write_to_file=True,
     #                                                                                                 save_path='assignment_3/models/VAE')
-    # #print("kl_losses: ",  train_kl_losses)
-
+    # print("kl_losses: ",  train_kl_losses)
+    # model, x_sample, output_decoder, train_loss, test_loss = train_ex_7_to_8.train(model,optimizer,30,train_loader,test_loader,save_path='assignment_3/models/VAE')
 
     # excercise 7a: get model output
-    #test_losses, output_list, latent_test, label_test = train_ex_7_to_8.test_model(model, criterion, test_loader, device)
+    # test_losses, output_list, latent_test, label_test = train_ex_7_to_8.test_model(model, criterion, test_loader, device)
 
 
-    #plot the loss
-    # train_ex_7_to_8.plot_loss(train_epoch_losses, train_reconstruction_loss, save_path='assignment_3/figures/excercise7a_total_loss.png')
+    # plot the loss
+    # train_ex_7_to_8.plot_loss(train_loss, test_loss, save_path='assignment_3/figures/excercise7a_smaller_loss.png')
     # train_ex_7_to_8.plot_kl_loss(train_kl_losses, save_path='assignment_3/figures/excercise7a_kl_loss.png')
 
 
@@ -149,9 +181,9 @@ if __name__ == "__main__":
     # print("shape output_tensor_test: ", np.shape(output_tensor_test))
     # print("shape latent_tensor_test: ", np.shape(latent_tensor_test))
     # print("shape label_tensor_test: ", np.shape(label_tensor_test))
-    #output_tensor_test = np.shape(output_tensor_test)
-    #latent_tensor_test = np.array(latent_tensor_test)
-    #label_tensor_test = np.shape(label_tensor_test)
+    # output_tensor_test = np.shape(output_tensor_test)
+    # latent_tensor_test = np.array(latent_tensor_test)
+    # label_tensor_test = np.shape(label_tensor_test)
 
     # print the first 10 digits of test set (0-9)
     # examples = enumerate(test_loader)
@@ -159,11 +191,11 @@ if __name__ == "__main__":
     # plot_images_exercise_7a(x_clean_example, output_tensor_test[:10])
 
     ### excercise 7b: latent space ###
-    #scatter_plot(latent_tensor_test, label_tensor_test)
+    # scatter_plot(latent_tensor_test, label_tensor_test)
 
-    ### excercise 7d: Decoder sample generation for VAE ###
-    # x_coords = np.linspace(0.3, 3, 15)
-    # y_coords = np.linspace(0.2, 5.5, 15)[::-1] # the [::-1] reverses the array so that the y-axis is flipped
+    # ## excercise 7d: Decoder sample generation for VAE ###
+    # x_coords = np.linspace(-2, 2, 15)
+    # y_coords = np.linspace(-2, 2, 15)[::-1] # the [::-1] reverses the array so that the y-axis is flipped
     # h0, h1 = np.meshgrid(x_coords, y_coords)
 
     # # create a grid of latent vectors. Each row is a latent vector (h0, h1) point in the (15, 15) grid
@@ -183,5 +215,9 @@ if __name__ == "__main__":
     # decoder_output = model.decoder(latent_grid_tensor).detach().cpu().numpy()
     # #print(np.shape(decoder_output))
 
-    # plot_mnist_grid_excercise_5(decoder_output, n_img_x=15, n_img_y=15,
+    # plot_mnist_grid_excercise_7d(decoder_output, n_img_x=15, n_img_y=15,
     #                             save_path='assignment_3/figures')
+
+    ### excercise 8a: Noisy image input to Variational auto-encoder ###
+    # plot the first 10 digits of test set (0-9)    
+    plot_images_exercise_8(model,test_loader)

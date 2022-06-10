7419020cacf89a354cb700f2dac15ff9e5576756
Tong <y.tong@student.tue.nl>
2022-06-10 09:07:31 +0200

    clean up code.

--- assignment_4/template_week1.py
+++ assignment_4/template_week1.py
@@ -27,6 +27,17 @@ import MNIST_dataloader
 # x_noisy_example = x_noisy_test[0:10,:,:,:]
 # labels_example = labels_test[0:10]
 
+def load_model(model, filename):
+    """ Load the trained model.
+    Args:
+        model (Model class): Untrained model to load.
+        filename (str): Name of the file to load the model from.
+    Returns:
+        Model: Model with parameters loaded from file.
+    """
+    model.load_state_dict(torch.load(filename))
+    return model
+
 def plot_examples(clean_images, noisy_images, ista_output, num_examples=10):
     """
     Plots some examples from the dataloader.
@@ -62,7 +73,6 @@ def plot_examples(clean_images, noisy_images, ista_output, num_examples=10):
     #plt.savefig("assignment_4/figures/exercise_1_b.png", dpi=300, bbox_inches='tight')
     plt.show()
 
-# %% ISTA
 def softthreshold(x,shrinkage):
     # x: torch.Size([32, 32])
     x_thx = x
@@ -78,6 +88,7 @@ def softthreshold(x,shrinkage):
     return x_thx
 
 def ISTA(mu,shrinkage,K,y):
+    # y: torch.Size([64, 1, 32, 32])
 
     A = np.identity(32)
     I = np.identity(32)
@@ -104,37 +115,84 @@ def ISTA(mu,shrinkage,K,y):
 
     return x_out
 
-class CNN_ex2(nn.Module):
+class LISTA(nn.Module):
     def __init__(self):
-        super(CNN_ex2,self).__init__()
-        self.conv = nn.Sequential(
+        super(LISTA,self).__init__()
+        self.conv1 = nn.Sequential(
         # input is N, 1, 32, 32
         nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3, 3), stride=1, padding=1), # N, 1, 32, 32
         nn.BatchNorm2d(1)       
         )
+        self.conv2 = nn.Sequential(
+        # input is N, 1, 32, 32
+        nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=(3, 3), stride=1, padding=1), # N, 1, 32, 32
+        #nn.BatchNorm2d(1)       
+        )
+    
+    def smoother_counter(self, x_out1, shrinkage_ex2):
+        # x_out1: torch.Size([64, 1, 32, 32])
+        x_smooth =  x_out1 + 0.5 * ((torch.sqrt(((x_out1 - shrinkage_ex2)**2) + 1))-(torch.sqrt(((x_out1 + shrinkage_ex2)**2) + 1)))
+    
+        return x_smooth
         
     def forward(self,x):
-        x = self.conv(x)
-        return x
+        unfolded_iterations = 3
+        shrinkage_ex2 = [1,2,3]
+        for i in range(unfolded_iterations):
+            if i == 0:
+                iter_out = 0
+            else:
+                iter_out = x_out3
 
-def smoother_counter(x,shrinkage_ex2):
-    # x: torch.Size([64, 1, 32, 32])
-    x_smooth =  x + 0.5 * ((torch.sqrt(((x - shrinkage_ex2)**2) + 1))-(torch.sqrt(((x + shrinkage_ex2)**2) + 1)))
-    
-    return x_smooth
+            x_out1 = self.conv1(x) + iter_out      
+            x_out2 = self.smoother_counter(x_out1,shrinkage_ex2[i])
+            x_out3 = self.conv2(x_out2)
+      
+        return x_out2
 
-def LISTA(model,unfolded_iterations,shrinkage_ex2,x_noisy):
-    for i in range(unfolded_iterations):
-        if i == 0:
-            iter_out = 0
-        else:
-            iter_out = x_out3
 
-        x_out1 = model(x_noisy) + iter_out      
-        x_out2 = smoother_counter(x_out1,shrinkage_ex2[i])
-        x_out3 = model(x_out2)
+def train_model(model, train_loader, n_epochs, optimizer, criterion):
+    """ Train the model.
+    Args:
+        model (Model class): Untrained model to train.
+        train_loader (DataLoader): DataLoader for training data.
+        n_epochs (int): Number of epochs to train for.
+    Returns:
+        Model: Trained model.
+    """
+    model.train()
+    loss_train = 0.0
+    train_loss = []
+    for epoch in range(n_epochs):
+        # go over all minibatches
+        for batch_idx,(x_clean, x_noisy, label) in enumerate(tqdm(train_loader)):
+           
+            if torch.cuda.is_available():
+                device = torch.device('cuda:0')
+                x_clean, x_noisy, label = [x.cuda() for x in [x_clean, x_noisy, label]]
+                model.to(device)
 
-    return x_out3  
+            x_out = model(x_noisy)
+            loss = criterion(x_clean, x_out)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+
+        train_loss.append(loss_train/len(train_loader))
+        print(f'Epoch {epoch+1}/{n_epochs} Loss: {loss_train/len(train_loader)}')
+    # save the trained model
+    torch.save(model.state_dict(), f"assignment_4/models/{epoch+1}.pth")
+
+    return model, train_loss
+
+def test_model(model, x_noisy_test):
+
+    model.eval()
+    x_out = model(x_noisy_test)
+    x_out = x_out.detach().numpy()
+    plot_examples(x_clean_test, x_noisy_test, x_out)
+
 
 if __name__ == "__main__":
     # set torches random seed
@@ -146,30 +204,27 @@ if __name__ == "__main__":
     mu = 0.9
     shrinkage = 0.1
     K = 1
-    # parameters for ex2
-    shrinkage_ex2 = [0.1,0.2,0.3]
-    n_epochs = 10
-    learning_rate = 0.1
-    unfolded_iterations = 3
-    # create the optimizer
-    model = CNN_ex2()
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), learning_rate, weight_decay=1e-5)
 
-    # # define the device
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #"cpu"
-    # print("Using device:", device)
-   
+    # parameters for ex2
+    n_epochs = 2
+    learning_rate = 0.1
+
+    model = LISTA()
+ 
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), learning_rate, weight_decay=1e-5) 
+
     # get dataloader
     train_loader, test_loader = MNIST_dataloader.create_dataloaders(data_loc, batch_size)
     # take first 10 images from clean test data set: torch.Size([10, 1, 32, 32])
     x_clean_test  = test_loader.dataset.Clean_Images[0:10]
     # take first 10 images from noisy test data set: torch.Size([10, 1, 32, 32])
     x_noisy_test  = test_loader.dataset.Noisy_Images[0:10]
- 
     
 
-  
+
+
+
     # #start timer
     # start_time = time.time()
     # #exercise 1 a
@@ -197,31 +252,17 @@ if __name__ == "__main__":
     # #exercise 1 b
     # plot_examples(x_clean_test, x_noisy_test, x_ista)
 
-    # #exercise 2a
+    # exercise 2a
 
-    #start timer
+    # train the model
+    # start timer
     start_time = time.time()
 
-    model.train()
-    loss_train = 0.0
-    train_loss = []
-    for epoch in range(n_epochs):
-        # go over all minibatches
-        for batch_idx,(x_clean, x_noisy, label) in enumerate(tqdm(train_loader)):
-            # fill in how to train your network using only the clean images
-            if torch.cuda.is_available():
-                device = torch.device('cuda:0')
-                x_clean, x_noisy, label = [x.cuda() for x in [x_clean, x_noisy, label]]
-                model.to(device)
+    #model, train_loss = train_model(model, train_loader, n_epochs, optimizer, criterion)
 
-            x_out = LISTA(model,unfolded_iterations,shrinkage_ex2,x_noisy)
-            loss = criterion(x_clean, x_out)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            loss_train += loss.item()
-        train_loss.append(loss_train/len(train_loader))
-        print(f'Epoch {epoch+1}/{n_epochs} Loss: {loss_train/len(train_loader)}')
+    # load the trained model
+    model = load_model(model, "assignment_4/models/10.pth")
+    test_model(model, x_noisy_test)
 
     print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
 
